879012811a1a3e05d4edf4312f43a168e50f152f
garrtanith <garrtanith@gmail.com>
2026-06-10 14:56:49 +0200

    Update database driver

--- database/connection.py
+++ database/connection.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
 
 
-DATABASE_URL = "postgresql+psycopg2://librarian:librarian123@localhost:5432/library"
+DATABASE_URL = "postgresql+psycopg://librarian:librarian123@localhost:5432/library"
 
 engine = create_engine(DATABASE_URL)
 
